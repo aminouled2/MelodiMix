678eab72786d66663085f2558818f5113419c5d2
Amin <59963622+aminouled2@users.noreply.github.com>
2023-05-24 21:20:55 +0200

    Initial Commit

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+const { execSync } = require('child_process');
+try {
+    const javaOutput = execSync('java src.main.java.Main').toString().trim();
+    console.log('Java output:', javaOutput);
+}
+catch (error) {
+    console.error('Java program execution failed:', error);
+}
